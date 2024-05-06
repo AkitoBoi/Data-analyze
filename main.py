8a10e5b352fb9d6b138595785095627cef943a47
Nikita Larchenko <142445793+AkitoBoi@users.noreply.github.com>
2024-05-06 13:14:02 +0300

    Removed unnecessary comments and redundant library

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 import os
 
 def get_file_path():
-    """
-    Запрашивает у пользователя путь к файлу и возвращает его.
-
-    Returns:
-    str: Путь к файлу.
-    """
     while True:
         file_path = input("Введите путь к файлу с данными: ")
         if os.path.isfile(file_path):
@@ -18,15 +11,6 @@ def get_file_path():
             print("Файл не найден. Пожалуйста, введите корректный путь к файлу.")
 
 def load_data(file_path):
-    """
-    Загружает данные из файла.
-
-    Parameters:
-    file_path (str): Путь к файлу с данными.
-
-    Returns:
-    pandas.DataFrame: Загруженные данные.
-    """
     try:
         data = pd.read_csv(file_path)
         return data
@@ -35,13 +19,6 @@ def load_data(file_path):
         return None
 
 def display_histogram(data, column_name):
-    """
-    Отображает гистограмму для указанного столбца данных.
-
-    Parameters:
-    data (pandas.DataFrame): Данные для визуализации.
-    column_name (str): Имя столбца данных.
-    """
     plt.hist(data[column_name])
     plt.xlabel('Значения')
     plt.ylabel('Частота')
@@ -49,13 +26,11 @@ def display_histogram(data, column_name):
     plt.show()
 
 def main():
-    # Загрузка данных
     file_path = get_file_path()
     data = load_data(file_path)
     if data is None:
         return
 
-    # Отображение гистограммы
     column_name = input("Введите имя столбца для построения гистограммы: ")
     display_histogram(data, column_name)
 
